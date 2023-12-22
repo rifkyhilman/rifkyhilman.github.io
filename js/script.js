d8756f5ea48202776d86adc32b09c36b73a8d4ca
rifkyhilman <rifkyhilman265@gmail.com>
2023-12-22 10:30:11 +0700

    :construction: Add animation scroll use Scrollreveal library
    add animation scroll use Scrollreveal library

--- js/script.js
+++ js/script.js
@@ -41,3 +41,17 @@ darkModeIcon.onclick = () => {
     darkModeIcon.classList.toggle('bx-sun');
     document.body.classList.toggle('dark-mode');
 };
+
+// ========== Scroll reveal ========== 
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.portofolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left'});
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right'});
+
