d4ab574f317ebea8d9aec28263bdafefe924d4fe
rifkyhilman <rifkyhilman265@gmail.com>
2023-12-22 10:19:21 +0700

    :lipstick: Add responsive and dark mode
    add responsive and dark mode

--- js/script.js
+++ js/script.js
@@ -2,18 +2,42 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+// ========== Scroll sections active link ========== 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
 
-// ========== Sticky navbar ========== 
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        };
+    });
+
+
+// ========== Sticky navbar ========== 
     let header = document.querySelector('.header');
     
     header.classList.toggle('sticky', window.scrollY > 90);
 };
 
-// ========== Scroll sections active link ========== 
-let sections = document.querySelector('section');
-let
+// ========== Dark Mode ========== 
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
